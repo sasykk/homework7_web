4cb3f633dc71ad7612a943abb619dd5abf9fd232
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 15:28:25 +0300

    Update query_3.sql

--- DB/query_3.sql
+++ DB/query_3.sql
@@ -1,1 +1,6 @@
-
+SELECT Groups.name AS group_name, AVG(grade) AS avg_grade
+FROM Grades
+JOIN Students ON Grades.student_id = Students.id
+JOIN Groups ON Students.group_id = Groups.id
+WHERE Grades.subject_id = <subject_id>
+GROUP BY Groups.name;
