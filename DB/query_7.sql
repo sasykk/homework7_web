c8da5b2c24b801f7af7d3096cb8a1d0f119148f4
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 15:30:03 +0300

    Update query_7.sql

--- DB/query_7.sql
+++ DB/query_7.sql
@@ -1,1 +1,4 @@
-
+SELECT Students.name AS student_name, Grades.grade
+FROM Students
+JOIN Grades ON Students.id = Grades.student_id
+WHERE Students.group_id = <group_id> AND Grades.subject_id = <subject_id>;
