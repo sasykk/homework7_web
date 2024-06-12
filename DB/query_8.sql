becc1c2c7b63da07d6dfb6d31e4117794f20b66a
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 15:30:27 +0300

    Update query_8.sql

--- DB/query_8.sql
+++ DB/query_8.sql
@@ -1,1 +1,5 @@
-
+SELECT Teachers.name AS teacher_name, AVG(Grades.grade) AS avg_grade
+FROM Teachers
+JOIN Subjects ON Teachers.id = Subjects.teacher_id
+JOIN Grades ON Subjects.id = Grades.subject_id
+GROUP BY Teachers.name;
