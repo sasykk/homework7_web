eedf12a77fefc9b98e086c7c24170709336a0424
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 15:47:51 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,48 @@
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime, Float
 
+engine = create_engine('sqlite:///mydatabase.db', echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    group_id = Column(Integer, ForeignKey('groups.id'))
+    grades = relationship('Grade', back_populates='student')
+
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    students = relationship('Student', back_populates='group')
+
+class Teacher(Base):
+    __tablename__ = 'teachers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    subjects = relationship('Subject', back_populates='teacher')
+
+class Subject(Base):
+    __tablename__ = 'subjects'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    teacher_id = Column(Integer, ForeignKey('teachers.id'))
+    teacher = relationship('Teacher', back_populates='subjects')
+    grades = relationship('Grade', back_populates='subject')
+
+class Grade(Base):
+    __tablename__ = 'grades'
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey('students.id'))
+    subject_id = Column(Integer, ForeignKey('subjects.id'))
+    grade = Column(Integer)
+    date_received = Column(DateTime, default=func.now())
+    student = relationship('Student', back_populates='grades')
+    subject = relationship('Subject', back_populates='grades')
+
+Base.metadata.create_all(engine)
