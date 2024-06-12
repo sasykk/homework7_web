d26946ba6dbeb58f6f42f61fadef99e9beb6b3d2
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 15:31:03 +0300

    Update query_9.sql

--- DB/query_9.sql
+++ DB/query_9.sql
@@ -1,1 +1,5 @@
-
+SELECT Subjects.name AS subject_name
+FROM Subjects
+JOIN Grades ON Subjects.id = Grades.subject_id
+JOIN Students ON Grades.student_id = Students.id
+WHERE Students.name = '<student_name>';
