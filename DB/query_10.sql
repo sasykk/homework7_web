309b8432f88335089078083cdd0dcb7d996374eb
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-06-12 15:27:03 +0300

    Update query_10.sql

--- DB/query_10.sql
+++ DB/query_10.sql
@@ -1,1 +1,6 @@
-
+SELECT Subjects.name AS subject_name
+FROM Subjects
+JOIN Grades ON Subjects.id = Grades.subject_id
+JOIN Students ON Grades.student_id = Students.id
+JOIN Teachers ON Subjects.teacher_id = Teachers.id
+WHERE Students.name = '<student_name>' AND Teachers.name = '<teacher_name>';
